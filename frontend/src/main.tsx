4fad7b86ee55eae4ca4fcb9ffaa44b2c9ec95375
barak7821 <barak7821@gmail.com>
2025-10-10 23:37:39 +0300

    Migrate project to TypeScript: update ESLint configuration, convert main and App files, adjust package.json dependencies, and create TypeScript configuration files.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './utils/AuthContext.jsx'
-import { CartProvider } from './utils/CartContext.jsx'
+import { AuthProvider } from './utils/AuthContext'
+import { CartProvider } from './utils/CartContext'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <CartProvider>
       <AuthProvider>
--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,4 +1,3 @@
-import React from "react"
 import NavBar from "../components/NavBar"
 import AboutCard from "../components/AboutCard"
 import Footer from "../components/Footer"
